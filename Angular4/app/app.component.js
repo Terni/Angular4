a7397f6ac7020148354c00442485f79ecbce8f00
Tomáš Prokop <tomas.prokop@wincor-nixdorf.cz>
2017-11-15 01:09:55 +0100

    change for css styles

--- Angular4/app/app.component.js
+++ Angular4/app/app.component.js
@@ -17,7 +17,7 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         templateUrl: 'app.component.html',
-        styleUrls: ['app.component.css'],
+        //styleUrls: [ 'app.component.css'],
         moduleId: module.id
     })
 ], AppComponent);
